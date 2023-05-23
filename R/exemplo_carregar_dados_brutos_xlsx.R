1bad8e0b29a66e495fe1cf7f50a5a71eac8036ce
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-05-23 17:48:27 -0300

    Update exemplo_carregar_dados_brutos_xlsx.R

--- R/exemplo_carregar_dados_brutos_xlsx.R
+++ R/exemplo_carregar_dados_brutos_xlsx.R
@@ -22,7 +22,7 @@ carregar_dados_brutos_xlsx <- function(
   return(dados)
 }
 
-#dados_brutos <- carregar_dados_brutos_xlsx()
+#dados_brutos <- carregar_dados_brutos_xlsx(caminho = "/home/usuario/Documentos/Vitor/Piper3D/WWF/Monitora/Dados/Monitora/Monitora/piper3d_monitora_florestal/data-raw/dados-brutos.xlsx")
 #dplyr::glimpse(dados_brutos)
 
 
