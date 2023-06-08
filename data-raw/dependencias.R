cdfb5775b2a98d5be1ed3f9374050e4329a5ec3e
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-06-08 07:50:23 -0300

    Create dependencias.R

---
+++ data-raw/dependencias.R
@@ -0,0 +1,22 @@
+
+# criar dependencias do pacote --------------------------------------------
+
+# usethis::use_package("Distance")
+usethis::use_package("dplyr")
+usethis::use_package("DT")
+# usethis::use_package("forcats")
+# usethis::use_package("ggpubr")
+# usethis::use_package("ggplot2")
+# usethis::use_package("here")
+# # usethis::use_package("janitor")
+# # usethis::use_package(lubridate")
+# usethis::use_package("mrds")
+# usethis::use_package("plotly")
+# usethis::use_package("purrr")
+# usethis::use_package("readr")
+# usethis::use_package("readxl")
+# # usethis::use_package("stringi")
+# # usethis::use_package("stringr")
+# # usethis::use_package("tidyr")
+# # usethis::use_package("tidyselect")
+# usethis::use_package("utils")
