a7288e38cb2d5ee2abf235a7d54d39af296c78e1
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-06-13 09:43:32 -0300

    Update testar_bondade_ajuste.R

--- R/testar_bondade_ajuste.R
+++ R/testar_bondade_ajuste.R
@@ -1,19 +1,41 @@
 # Documentacao da funcao testar_bondade_ajuste() --------------------------
-#' Title
+#' Teste de bondade de ajuste e plot de quantis
 #'
 #' @description
-#' A short description...
+#' A função `testar_bondade_ajuste()` realiza o teste de bondade de ajsute dos
+#' modelos de função de detecção. Para dados contínuos de distância, são
+#' utilizados os testes de Kolmogorov-Smrinov a Cramer-von Mises. Quando os
+#' dados de distância estão categorizados o teste $X{^2}$ pode ser usado.
 #'
-#' @param dados breve descrição sobre o argumento
-#' @param plot breve descrição sobre o argumento
-#' @param chisq breve descrição sobre o argumento
-#' @param nc breve descrição sobre o argumento
-#' @param intervalos_distancia breve descrição sobre o argumento
+#'  @usage testar_bondade_ajuste(
+#'            dados,
+#'            plot = TRUE,
+#'            chisq = FALSE,
+#'            nboot = 100,
+#'            ks = FALSE,
+#'            nc = NULL,
+#'            intervalos_distancia = NULL
+#'         )
 #'
-#' @details
-#' Additional details...
+#' @param dados recebe uma lista nomeada gerada pela função
+#'  [gerar_lista_modelos_selecionados()] contendo os modelos de função de
+#'  detecção
+#' @param plot lógico, se `TRUE` retorna o plot de quantis teóricos x  empíricos
+#' para cada modelo
+#' @param chisq lógico, se `TRUE` retorna a estatística $X{^2}$ mesmo para
+#' modelos que usam dados contínuos de distância. Para dados de distância
+#' categorizados a estatística $X{^2}$ é automaticamente utilizada
+#' @param nboot número de réplicas usados para calcular os p-valores no teste
+#' de ajuste de bondade de Kolmogorov_Smirnov
+#' @param ks lógico, se `TRUE` realiza o teste de Kolmogorov-Smirnov (como
+#' envolve o uso de bootstrap pode levar mais tempo). Por configuração,
+#' `ks = FALSE` realizando o teste de Cramer-von Mises
+#' @param nc número de classes de distâncias igualmente espaçadas para o teste
+#' de quiquadrado quando `chisq = TRUE`
+#' @param intervalos_distancia vetor de pontos de corte utilizado para
+#' categorização das distâncias
 #'
-#' @return breve descrição sobre o objeto de saída
+#' @return retorna uma lista contendo objetos da classe `dsmodel`
 #' @export
 #'
 #' @examples
@@ -25,7 +47,7 @@
 #'   nome_sps = "dasyprocta_croconota",
 #'   validacao_obs = "especie"
 #' ) |>
-#'   transformar_dados_formato_Distance(amostras_repetidas = FALSE)
+#'   transformar_dados_formato_Distance()
 #'
 #' # ajustar modelos com funções chave diferentes
 #' # modelo Half-normal
@@ -49,21 +71,31 @@
 #'     truncamento = 15
 #'   )
 #'
-#' # gerar lista contendo todos os modelos ajsutados aos dados
-#' lista_modelos_ajustados <- list(
-#'   `half-normal` = modelo_hn,
-#'   `hazard-rate` = modelo_hr,
-#'   `uniforme` = modelo_unif
+#' # gerar a tabela de seleção com o resumo comparativo dos modelos
+#' modelos_selecionados <- selecionar_funcao_deteccao_termo_ajuste(
+#'   modelo_hn$`Sem termo`,
+#'   modelo_hn$Cosseno,
+#'   modelo_hn$`Hermite polinomial`,
+#'   modelo_hr$`Sem termo`,
+#'   modelo_hr$Cosseno,
+#'   modelo_hr$`Polinomial simples`,
+#'   modelo_unif$Cosseno,
+#'   modelo_unif$`Polinomial simples`
 #' )
 #'
-#' # gerar a tabela de seleção com o resumo comparativo dos modelos
-#' selecao_funcao_deteccao_termo_ajuste <- selecionar_funcao_deteccao_termo_ajuste(
-#'   lista_modelos_ajustados
+#' # gerar uma lista com os modelos selecionados ordenados do melhor para o pior modelo
+#' lista_modelos_selecionados <- gerar_lista_modelos_selecionados(
+#' modelo_hr$`Sem termo`,
+#' modelo_hn$Cosseno,
+#' modelo_unif$Cosseno,
+#' modelo_unif$`Polinomial simples`,
+#' modelo_hn$`Sem termo`,
+#' nome_modelos_selecionados = selecao_funcao_deteccao_termo_ajuste
 #' )
 #'
 #' # teste de bondade de ajuste dos modelos e Q-Q plots
 #' bondade_ajuste <- testar_bondade_ajuste(
-#'   modelos_selecionados,
+#'   lista_modelos_selecionados,
 #'   plot = TRUE,
 #'   chisq = TRUE,
 #'   intervalos_distancia =  NULL
@@ -74,6 +106,8 @@ testar_bondade_ajuste <- function(
     dados,
     plot = FALSE,
     chisq = FALSE,
+    nboot = 100,
+    ks = FALSE,
     nc = NULL,
     intervalos_distancia = NULL
 ) {
@@ -90,6 +124,8 @@ testar_bondade_ajuste <- function(
           x,
           plot = plot,
           chisq = chisq,
+          nboot = nboot,
+          ks = ks,
           nc = nc,
           breaks = intervalos_distancia
         )
@@ -113,6 +149,8 @@ testar_bondade_ajuste <- function(
           x,
           plot = plot,
           chisq = chisq,
+          nboot = nboot,
+          ks = ks,
           nc = nc,
           breaks = intervalos_distancia
         )
@@ -133,6 +171,8 @@ utils::globalVariables(
     "x",
     "plot",
     "chisq",
+    "nboot",
+    "ks",
     "nc",
     "breaks",
     "intervalos_distancia",
