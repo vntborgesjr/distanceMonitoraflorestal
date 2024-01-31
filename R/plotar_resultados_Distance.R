13e052278831c1c7bd99378c6c854147497f9f7b
Vitor Borges Jr <vntborgesjr@gmail.com>
2024-01-31 20:01:06 -0300

    Create plotar_resultados_Distance.R
    cria função para plotar os resultados de estimativa de abundância e de densidade gerados a partir de uma única função de detecção

---
+++ R/plotar_resultados_Distance.R
@@ -0,0 +1,150 @@
+# Documentacao da funcao plotar_resultados_Distance() --------
+#' Plota os resultados de abundância ou densidade estimada
+#'
+#' @param dados recebe um objeto do tipo `data.frame` gerado pela função
+#' [gerar_resultados_Distance()]
+#' @param tipo_de_resultado ...
+#' @param intervalo_confianca ...
+#'
+#' @description
+#' Descrever o que cada opção retorna
+#'
+#' @return Retorna ...
+#' @export
+#'
+#' @examples \dontrun{
+#'
+#' }
+plotar_resultados_Distance <- function(
+    dados,
+    tipo_de_resultado = c("abundância", "densidade"),
+    intervalo_confianca = FALSE
+) {
+
+  # definir o tipo de resultado fornecido pelo usuário
+  tipo_de_resultado <- match.arg(tipo_de_resultado)
+
+  if (tipo_de_resultado == "abundância") {
+    if (intervalo_confianca == FALSE) {
+      grafico <- dados |>
+        dplyr::mutate(Ano = as.integer(Ano)) |>
+        dplyr::group_by(
+          Modelo,
+          Ano
+        ) |>
+        dplyr::summarise(`Abundância estimada` = sum(`Abundância estimada`)) |>
+        ggplot2::ggplot() +
+        ggplot2::aes(
+          x = Ano,
+          y = `Abundância estimada`
+        ) +
+        ggplot2::geom_line() +
+        ggplot2::scale_x_continuous(breaks = 2014:2021) +
+        # ggplot2::facet_wrap(facets = ggplot2::vars(Modelo)) +
+        ggplot2::theme_bw()
+
+    } else {
+
+      print("Não há estimativa de intervalo de confiança para a abundância estimada")
+      # grafico <- dados |>
+      #   dplyr::mutate(Ano = as.integer(Ano)) |>
+      #   dplyr::group_by(
+      #     Modelo,
+      #     Ano
+      #   ) |>
+      #   dplyr::summarise(`Abundância estimada` = sum(`Abundância estimada`)) |>
+      #   ggplot2::ggplot() +
+      #   ggplot2::aes(
+      #     x = Ano,
+      #     y = `Abundância estimada`
+      #   ) +
+      #   ggplot2::geom_line() +
+      #   ggplot2::scale_x_continuous(breaks = 2014:2021) +
+      #   # ggplot2::facet_wrap(facets = ggplot2::vars(Modelo)) +
+      #   ggplot2::theme_bw() +
+      #   ggplot2::geom_line(
+      #     ggplot2::aes(
+      #       x = Ano,
+      #       y = `Abundância estimada` - `Intervalo de confiança inferior`
+      #     ),
+      #     linetype = 3
+      #   )  +
+      #   ggplot2::geom_line(
+      #     ggplot2::aes(
+      #       x = Ano,
+      #       y = `Abundância estimada` + `Intervalo de confiança superior`
+      #     ),
+      #     linetype = 3
+      #   )
+
+    }
+  } else {
+    if (intervalo_confianca == FALSE) {
+
+      grafico <- dados |>
+        dplyr::filter(Rotulo != "Total") |>
+        dplyr::mutate(Ano = as.integer(Rotulo)) |>
+        dplyr::group_by(
+          Modelo,
+          Ano
+        ) |>
+        dplyr::summarise(`Abundância estimada` = sum(`Abundância estimada`)) |>
+        ggplot2::ggplot() +
+        ggplot2::aes(
+          x = Ano,
+          y = `Estimativa de densidade (ind/ha)`
+        ) +
+        ggplot2::geom_line() +
+        ggplot2::scale_x_continuous(breaks = 2014:2021) +
+        # ggplot2::facet_wrap(facets = ggplot2::vars(Modelo)) +
+        ggplot2::theme_bw()
+
+    } else {
+
+      grafico <- dados |>
+        dplyr::filter(Rotulo != "Total") |>
+        dplyr::mutate(Ano = as.integer(Rotulo)) |>
+        dplyr::group_by(
+          Modelo,
+          Ano
+        ) |>
+        dplyr::summarise(n = sum(`Abundância estimada`)) |>
+        ggplot2::ggplot() +
+        ggplot2::aes(
+          x = Ano,
+          y = `Estimativa de densidade (ind/ha)`
+        ) +
+        ggplot2::geom_line() +
+        ggplot2::scale_x_continuous(breaks = 2014:2021) +
+        # ggplot2::facet_wrap(facets = ggplot2::vars(Modelo)) +
+        ggplot2::theme_bw() +
+        ggplot2::geom_line(
+          ggplot2::aes(
+            x = Ano,
+            y = `Estimativa de densidade (ind/ha)` - `Intervalo de confiança inferior`
+          ),
+          linetype = 3
+        )  +
+        ggplot2::geom_line(
+          ggplot2::aes(
+            x = Ano,
+            y = `Estimativa de densidade (ind/ha)` + `Intervalo de confiança superior`
+          ),
+          linetype = 3
+        )
+
+    }
+  }
+
+  return(grafico)
+
+}
+
+
+utils::globalVariables(
+  c(
+    "tipo_de_resultado",
+    "dados",
+    "grafico"
+  )
+)
