b4cc4db35315861c643ba9f6830ff33e8612b0b8
Vitor Borges Jr <vntborgesjr@gmail.com>
2025-07-04 16:37:37 -0300

    Update monitora_aves_masto_florestal.R
    atualizado  número de linhas colunas base de dados

--- R/monitora_aves_masto_florestal.R
+++ R/monitora_aves_masto_florestal.R
@@ -5,20 +5,18 @@
 #' Traz os dados de aves e mamiferos de medio e grande porte obtidos por amostragem por distancia em 40 Unidades de Conservacao do Brasil entre os anos de 2014 e 2022.
 #'
 #' @format ## 'monitora_aves_masto_florestal'
-#' Uma \code{tibble} que contem 26.420 linhas de 19 colunas:
+#' Uma \code{tibble} que contem 27.887 linhas de 22 colunas:
 #' \describe{
-#'   \item{nome_ea}{nome da Estacao Amostral}
-#'   \item{n_visitas_repetidas}{numero total de visitas realizadas em cada Estacao Amostral}
-#'   \item{codigo_uc}{codigo da Unidade de Conservacao}
 #'   \item{categoria_uc}{categoria da Unidade de Conservacao}
 #'   \item{nome_uc}{nome da Unidade de Conservacao}
 #'   \item{nome_uc_abv}{nome da Unidade de Conservacao abreviado}
+#'   \item{codigo_uc}{codigo da Unidade de Conservacao}
+#'   \item{nome_ea}{nome da Estacao Amostral}
 #'   \item{numero_ea}{numero da Estacao Amostral dentro de uma Unidade de Conservacao}
 #'   \item{data_amostragem}{ano, mes e dia em que a observacao foi realizada}
 #'   \item{estacao}{estacao do ano em que a observacao foi realizada}
 #'   \item{ano}{ano em que a observacao foi realizada}
 #'   \item{esforco_dia}{distancia total percorrida em metros em um dia da amostragem na Estacao Amostral equivalente}
-#'   \item{esforco_total}{distancia total percorrida em metros em todos os dias de amostragem na Estacao Amostral Equivalente}
 #'   \item{nome_classe}{nome da classe ao qual o individuo observado pertence}
 #'   \item{nome_ordem}{nome da ordem ao qual o individuo observado pertence}
 #'   \item{nome_familia}{nome do família ao qual o individuo observado pertence}
@@ -28,9 +26,9 @@
 #'   \item{validacao}{nivel taxonomico de validacao do individuo observado}
 #'   \item{distancia}{distancia perpendicular em relacao ao amostrador na qual o individuo foi observado}
 #'   \item{tamanho_grupo}{numero de individuos registrados na mesma observacao}
-#'   \item{numero_observadores}{numero de observadores que precorriam o transecto na ocasiao da observacao}
 #'   \item{velocidade_km_h}{velocidade média do percurso do transecto}
 #'   \item{tempo_censo}{tempo total de duracao do percurso de uma estacao amostral}
+#'   \item{numero_observadores}{numero de observadores que precorriam o transecto na ocasiao da observacao}
 #' }
 #'
 #' @source https://www.gov.br/icmbio/pt-br/assuntos/monitoramento/conteudo/dados
